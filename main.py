2f8ed8fc251038e96a43a7559a0e6fcbaa37dea8
Matheus Alves <matheus.git@teste.com>
2026-09-02 08:30:37 -0300

    Adicionado bibliotecas, pequena alteração no main e vendo se funciona no FastApi

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print ("Controle de gastos")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Despesa(BaseModel):
+    descricao: str
+    valor: float
+    categoria: str
+
+
+despesas = []
+
+
+@app.get("/")
+def inicio():
+    return {"mensagem": "Controle de Gastos API funcionando!"}
+
+
+@app.post("/despesas")
+def cadastrar_despesa(despesa: Despesa):
+    despesas.append(despesa)
+    return despesa
+
+
+@app.get("/despesas")
+def listar_despesas():
+    return despesas
